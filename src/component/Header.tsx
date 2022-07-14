f40b1e2186739646f93827bdac18d8951a3b545f
kimk1029ghp_XD8HMwBh9p4x8lGEh92MvyqdVcWGaC0NlQb <kimk1029@naver.com>
2022-07-14 18:31:28 +0900

    init

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,4 +1,43 @@
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+import LOGO from "../assets/img/logo.png";
+const SHeader = styled.nav`
+  background-color: #ff7f42;
+  width: 100%;
+  height: 80px;
+  .container {
+    width: 1024px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    .logo {
+      height: 40px;
+    }
+  }
+`;
+const SLoginBtn = styled.div`
+  background: #ffffff;
+  border-radius: 10px;
+  color: #000;
+  width: 76px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
 const Header = () => {
-  return <nav>Header</nav>;
+  return (
+    <SHeader>
+      <div className="container">
+        <img className="logo" src={LOGO} />
+        <div className="account">
+          <SLoginBtn>로그인</SLoginBtn>
+        </div>
+      </div>
+    </SHeader>
+  );
 };
 export default Header;
--- src/view/Main.tsx
+++ src/view/Main.tsx
@@ -1,3 +1,77 @@
+import styled from "styled-components";
+import Header from "../component/Header";
+import IMG_MAINLOGO from "../assets/img/logo-main.png";
+import BANNER_1 from "../assets/img/banner/banner-1.png";
+import ICON_SEARCH from "../assets/img/icon/search.svg";
+const SMainContainer = styled.div`
+  .container {
+    width: 1024px;
+    margin: 0 auto;
+    .main {
+      width: 500px;
+      margin: 0 auto;
+      padding: 130px 0;
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .main-logo {
+        width: 225px;
+        margin: 20px auto;
+      }
+      .banner {
+        width: 100%;
+      }
+    }
+  }
+`;
+const SSearchInput = styled.div`
+  width: 100%;
+  margin: 60px 0;
+  position: relative;
+  input {
+    background: #fafafa;
+    filter: drop-shadow(0px 0px 9px rgba(34, 34, 34, 0.34));
+    border-radius: 10px;
+    border: 0;
+    width: 100%;
+    height: 44px;
+    padding: 0 20px;
+    box-sizing: border-box;
+    font-size: 18px;
+    &:focus {
+      outline: none;
+    }
+  }
+  .searchBtn {
+    cursor: pointer;
+    width: 46px;
+    height: 46px;
+    background: #ff7f42;
+    border-radius: 0px 10px 10px 0px;
+    position: absolute;
+    right: -1px;
+    top: -1px;
+    background-image: url(${ICON_SEARCH});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 24px;
+  }
+`;
 export const Main = () => {
-  return <div>ddd</div>;
+  return (
+    <SMainContainer>
+      <Header />
+      <div className="container">
+        <div className="main">
+          <img className="main-logo" src={IMG_MAINLOGO} />
+          <img className="banner" src={BANNER_1} />
+          <SSearchInput>
+            <input />
+            <div className="searchBtn"></div>
+          </SSearchInput>
+        </div>
+      </div>
+    </SMainContainer>
+  );
 };

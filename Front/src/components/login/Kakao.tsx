825aca9fc1e13ff005abd5a4de553d73ad03bc25
장현웅 <jhw811@gmail.com>
2022-10-26 19:29:32 +0900

    카카오 로그인 임시

--- Front/src/components/login/Kakao.tsx
+++ Front/src/components/login/Kakao.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
+import KakaoLogin from "react-kakao-login";
 import styled from "styled-components";
 
+const token = "64d0c649f5b7e5a3ef9790587ee5f19b";
 export const Kakao = () => {
   return (
-    <KakaoLoginButton onClick={() => console.log("카카오 로그인")}>
-      <Image src={"/img/icon/kakao.svg"} width={14} height={12} />
-      카카오 로그인
-    </KakaoLoginButton>
+    <KakaoLogin
+      token={token}
+      onSuccess={console.log}
+      onFail={console.error}
+      onLogout={console.info}
+      render={({ onClick }: any) => {
+        return (
+          <KakaoLoginButton onClick={onClick}>
+            <Image src={"/img/icon/kakao.svg"} width={14} height={12} />
+            카카오 로그인
+          </KakaoLoginButton>
+        );
+      }}
+    />
   );
 };
 

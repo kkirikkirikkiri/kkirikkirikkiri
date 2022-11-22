fce1a45ee7e0b1318927b647e3d36a992d78f667
장현웅 <jhw811@gmail.com>
2022-11-22 11:00:22 +0900

    init

--- Front/next.config.js
+++ Front/next.config.js
@@ -8,8 +8,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  //github pages에 url때문에 임시로 적용 나중에 지워야함
-  // basePath: "/kkiri",
+  basePath: "/kkiri",
 };
 
 module.exports = nextConfig;
